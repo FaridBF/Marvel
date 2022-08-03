53c71015b159a27d4023b260e01c8f4f682e8d91
Farid <bouras.farid@outlook.fr>
2022-08-03 14:07:58 +0200

    feat: route created  "characters"

---
+++ routes/characters.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/characters', async (req, res) => {
+  console.log(req.params);
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+router.get('/character/:characterId', async (req, res) => {
+  console.log(req.params);
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/character/5fcf91f4d8a2480017b91453?apiKey=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+module.exports = router;
