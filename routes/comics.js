222e3603a4e027aea7c34fe63d57e654dc145c36
Farid <bouras.farid@outlook.fr>
2022-08-03 14:08:16 +0200

    feat: route created  "comics"

---
+++ routes/comics.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/comics', async (req, res) => {
+  console.log(req.params);
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+router.get('/comics/:characterId', async (req, res) => {
+  console.log(req.params);
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/comics/5fc8ba1fdc33470f788f88b3?apiKey=${process.env.API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+module.exports = router;
